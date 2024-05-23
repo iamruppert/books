acd4cdb3f94cd5d052aca2dba0a1b5a8031aa28a
iamruppert <lukaszziolkiewicz@tutanota.com>
2024-05-24 01:52:06 +0200

    init

---
+++ src/main/java/com/lukasz/stephen_king/StephenKingApplication.java
@@ -0,0 +1,13 @@
+package com.lukasz.stephen_king;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StephenKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StephenKingApplication.class, args);
+	}
+
+}

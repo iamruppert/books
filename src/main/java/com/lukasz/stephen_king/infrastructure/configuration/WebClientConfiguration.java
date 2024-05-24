105e5fa733cfec872dd7f8929e132e42fc3f351b
iamruppert <lukaszziolkiewicz@tutanota.com>
2024-05-24 02:53:04 +0200

    Add WebClient configuration

---
+++ src/main/java/com/lukasz/stephen_king/infrastructure/configuration/WebClientConfiguration.java
@@ -0,0 +1,54 @@
+package com.lukasz.stephen_king.infrastructure.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class WebClientConfiguration {
+
+    public static final String STEPHEN_KING_URL = "https://stephen-king-api.onrender.com/";
+    public static final int TIMEOUT = 5000;
+
+    @Bean
+    public WebClient webClient(final ObjectMapper objectMapper){
+        final var httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT)
+                .responseTimeout(Duration.ofMillis(TIMEOUT))
+                .doOnConnected(conn->
+                        conn.addHandlerLast(new ReadTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(TIMEOUT, TimeUnit.MILLISECONDS)));
+
+        final var strategies = ExchangeStrategies
+                .builder()
+                .codecs(configurer -> {
+                    configurer
+                            .defaultCodecs()
+                            .jackson2JsonEncoder(
+                                    new Jackson2JsonEncoder(objectMapper, MediaType.APPLICATION_JSON));
+                    configurer
+                            .defaultCodecs()
+                            .jackson2JsonDecoder(
+                                    new Jackson2JsonDecoder(objectMapper, MediaType.APPLICATION_JSON));
+                }).build();
+
+        return WebClient.builder()
+                .baseUrl(STEPHEN_KING_URL)
+                .exchangeStrategies(strategies)
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+}

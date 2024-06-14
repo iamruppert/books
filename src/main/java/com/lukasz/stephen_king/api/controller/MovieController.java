df308821306cb06bf521c5e4272a4794e86e7a46
iamruppert <lukaszziolkiewicz@tutanota.com>
2024-06-14 17:38:20 +0200

    Create endpoint in controller for getting data from API

---
+++ src/main/java/com/lukasz/stephen_king/api/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.lukasz.stephen_king.api.controller;
+
+import com.lukasz.stephen_king.buisness.MovieService;
+import com.lukasz.stephen_king.domain.MovieDomain;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/tmdb")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping("/movies")
+    public List<MovieDomain> getStephenKingMovies() {
+        return movieService.getStephenKingMovies();
+    }
+
+}

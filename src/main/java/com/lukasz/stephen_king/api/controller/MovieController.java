6f72b1b482ef918c100f02647b720bbf9132ce7e
iamruppert <lukaszziolkiewicz@tutanota.com>
2024-06-15 17:23:11 +0200

    Add endpoint to controller for getting movie details

--- src/main/java/com/lukasz/stephen_king/api/controller/MovieController.java
+++ src/main/java/com/lukasz/stephen_king/api/controller/MovieController.java
@@ -1,9 +1,11 @@
 package com.lukasz.stephen_king.api.controller;
 
 import com.lukasz.stephen_king.buisness.MovieService;
+import com.lukasz.stephen_king.domain.MovieDetailsDomain;
 import com.lukasz.stephen_king.domain.MovieDomain;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +23,8 @@ public class MovieController {
         return movieService.getStephenKingMovies();
     }
 
+        @GetMapping("/movie/{id}")
+    public MovieDetailsDomain getMovieDetails(@PathVariable int id) {
+        return movieService.getMovieDetails(id);
+    }
 }
